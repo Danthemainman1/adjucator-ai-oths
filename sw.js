082e90fd590103a26055f2a2a7f3526efa105623
Daniel Alipour <w217679524@student.hccs.edu>
2025-11-21 18:44:00 +0000

    Fix: PWA Manifest and Service Worker Registration

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'adjucator-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn-icons-png.flaticon.com/512/2643/2643509.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
